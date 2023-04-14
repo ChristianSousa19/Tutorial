d2f3a6c412fa180b2f69fba6f920385f87f33d0e
ChristianSousa19 <christianssv37@gmail.com>
2023-04-14 14:39:10 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int a=5;
+int b=10;
+if(a+b<10)
+ Console.WriteLine("É maior que dez");
+ else
+ Console.WriteLine("É menor que dez");
+ for(int counter = 0; counter < 10; counter++)
+{
+  Console.WriteLine($"Hello World! The counter is {counter}");
+
+}
+Console.WriteLine("Qual e o seu nome?");
+string nome = Console.ReadLine();
+Console.WriteLine("Olá é um prazer te conhecer  " + nome);
